bea316ee49bbead96445070a4f03d2c601e0b059
NICOABBINK-PC\NicoAbbink <studio.shapes@outlook.com>
2022-03-25 19:37:01 +0100

    file update

--- src/Components/pokemanCard.svelte
+++ src/Components/pokemanCard.svelte
@@ -1,12 +1,11 @@
-<script>	
-import {fade} from 'svelte/transition';
-export let pokeman;
+<script>
+	export let pokeman;
 </script>
 
 <a
 	class="p6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
-	href={`/pokemon/${pokeman.id}`} transition:fade>
-
+	href={`/pokemon/${pokeman.id}`}
+>
 	<img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
-	<h2 class="uppercase xl">{pokeman.id}. {pokeman.name}</h2>
+	<h2 class="uppercase xl">{pokeman.id}.{pokeman.name}</h2>
 </a>
